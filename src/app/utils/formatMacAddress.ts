21eb4b7f2815051de3cd656fb68147514bac850b
mayining <mayining@sensetime.com>
2024-07-11 13:47:03 +0800

    fix eslint error

--- src/app/utils/formatMacAddress.ts
+++ src/app/utils/formatMacAddress.ts
@@ -19,10 +19,14 @@ export const formatMacAddress = (value: string): string => {
   // Check if it's a standard MAC address (6 bytes) or an IPoIB MAC address (20 bytes)
   if (hexValues.length === 12) {
     // Standard MAC address
-    return hexValues.replace(/([0-9A-Fa-f]{2})(?=(?:[0-9A-Fa-f]{2})+(?!$))/g, "$1:").substring(0, 17);
+    return hexValues
+      .replace(/([0-9A-Fa-f]{2})(?=(?:[0-9A-Fa-f]{2})+(?!$))/g, "$1:")
+      .substring(0, 17);
   } else if (hexValues.length === 40) {
     // IPoIB MAC address
-    return hexValues.replace(/([0-9A-Fa-f]{2})(?=(?:[0-9A-Fa-f]{2})+(?!$))/g, "$1:").substring(0, 59);
+    return hexValues
+      .replace(/([0-9A-Fa-f]{2})(?=(?:[0-9A-Fa-f]{2})+(?!$))/g, "$1:")
+      .substring(0, 59);
   }
 
   // If not matching the lengths, return standard MAC address
